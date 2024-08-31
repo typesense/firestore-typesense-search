118f1667df425116d9558f8e85725b48bfecc011
Hayden <hayden.hhoang@gmail.com>
2024-08-31 08:57:44 +0700

    ci: fix parsing geopoint data type test
    - use typesense v27.0 image
    - add prettierrc

--- functions/src/utils.js
+++ functions/src/utils.js
@@ -1,27 +1,40 @@
 const config = require("./config.js");
 
 const mapValue = (value) => {
-  const isObject = typeof value === 'object';
+  const isObject = typeof value === "object";
   const notNull = value !== null;
-  const length = Object.keys(value).length;
+  const length = isObject && notNull ? Object.keys(value).length : 0;
 
   const latitude = value.latitude ?? value.lat;
   const longitude = value.longitude ?? value.lng;
   const hasGeohashField = value.geohash != null && length == 3;
-  const isGeopointType = latitude != null && longitude != null && (length == 2 || hasGeohashField);
+  const isGeopointType =
+    latitude != null && longitude != null && (length == 2 || hasGeohashField);
 
-  if (isObject && notNull && value.seconds != null && value.nanoseconds != null) {
+  if (
+    isObject &&
+    notNull &&
+    value.seconds != null &&
+    value.nanoseconds != null
+  ) {
     // convert date to Unix timestamp
     // https://typesense.org/docs/0.22.2/api/collections.html#indexing-dates
     return Math.floor(value.toDate().getTime() / 1000);
   } else if (isObject && notNull && isGeopointType) {
     return [latitude, longitude];
-  } else if (isObject && notNull && value.firestore != null && value.path != null) {
-    return {"path": value.path};
+  } else if (
+    isObject &&
+    notNull &&
+    value.firestore != null &&
+    value.path != null
+  ) {
+    return {path: value.path};
   } else if (Array.isArray(value)) {
     return value.map(mapValue);
   } else if (isObject && notNull) {
-    return Object.fromEntries(Object.entries(value).map(([key, value]) => [key, mapValue(value)]));
+    return Object.fromEntries(
+        Object.entries(value).map(([key, value]) => [key, mapValue(value)]),
+    );
   } else {
     return value;
   }
@@ -46,7 +59,9 @@ exports.typesenseDocumentFromSnapshot = async (
   }
 
   // Build a document with just the fields requested by the user, and mapped from Firestore types to Typesense types
-  const mappedDocument = Object.fromEntries(entries.map(([key, value]) => [key, mapValue(value)]));
+  const mappedDocument = Object.fromEntries(
+      entries.map(([key, value]) => [key, mapValue(value)]),
+  );
 
   // using flat to flatten nested objects for older versions of Typesense that did not support nested fields
   // https://typesense.org/docs/0.22.2/api/collections.html#indexing-nested-fields
