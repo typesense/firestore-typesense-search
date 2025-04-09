92ab8946a6877452b19bd47b8ce2054e5b72b59e
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-09 14:13:23 +0300

    feat(buffering): add buffering option to `indexOnWrite`

--- functions/src/indexOnWrite.js
+++ functions/src/indexOnWrite.js
@@ -3,10 +3,53 @@ const config = require("./config.js");
 const utils = require("./utils.js");
 const createTypesenseClient = require("./createTypesenseClient.js");
 const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+const admin = require("firebase-admin");
 
 exports.indexOnWrite = onDocumentWritten(`${config.firestoreCollectionPath}/{docId}`, async (snapshot, _) => {
   const typesense = createTypesenseClient();
 
+  if (config.typesenseUseBuffer) {
+    return await bufferedWrites(snapshot);
+  }
+
+  return await realTimeWrites(snapshot, typesense);
+});
+
+const bufferedWrites = async (snapshot) => {
+  if (snapshot.data.after.data() == null) {
+    // Delete
+    const documentId = snapshot.data.before.id;
+    debug(`Buffering delete for document ${documentId}`);
+
+    return await admin.firestore().collection(config.typesenseBufferCollectionInFirestore).add({
+      documentId: documentId,
+      type: "delete",
+      status: "pending",
+      timestamp: Date.now(),
+      retries: 0,
+      pathParams: snapshot.params,
+    });
+  } else {
+    // Create / update
+    const latestSnapshot = await snapshot.data.after.ref.get();
+    const document = latestSnapshot.data();
+    const documentId = latestSnapshot.id;
+
+    debug(`Buffering upsert for document ${documentId}`);
+
+    return await admin.firestore().collection(config.typesenseBufferCollectionInFirestore).add({
+      documentId: documentId,
+      document: document,
+      type: "upsert",
+      status: "pending",
+      timestamp: Date.now(),
+      retries: 0,
+      pathParams: snapshot.params,
+    });
+  }
+};
+
+const realTimeWrites = async (snapshot, typesense) => {
   if (snapshot.data.after.data() == null) {
     // Delete
     const documentId = snapshot.data.before.id;
@@ -26,4 +69,4 @@ exports.indexOnWrite = onDocumentWritten(`${config.firestoreCollectionPath}/{doc
     }
     return await typesense.collections(config.typesenseCollectionName).documents().upsert(typesenseDocument);
   }
-});
+};
