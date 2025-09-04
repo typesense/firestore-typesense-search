1b2b93639cce98cb7413f0eb33f8ac8ab365072e
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-09-04 17:14:56 +0300

    feat: export all generated index on write functions dynamically

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 
-const {indexOnWrite} = require("./indexOnWrite.js");
-exports.indexOnWrite = indexOnWrite;
+// import all functions from indexOnWrite.js (including dynamically generated ones)
+const indexOnWriteModule = require("./indexOnWrite.js");
+
+// export all functions from indexOnWrite module
+Object.keys(indexOnWriteModule).forEach((key) => {
+  exports[key] = indexOnWriteModule[key];
+});
+
 exports.backfill = require("./backfill.js");
